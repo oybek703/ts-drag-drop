5329b329420f94448bfeab91e1af137a449e5c89
oybek703 <hhoybek@gmail.com>
2021-01-20 20:55:53 +0500

    advanced types finished

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,6 @@
 "use strict";
-var Human = /** @class */ (function () {
-    function Human(name) {
-        this.name = name;
-        if (name) {
-            this.name = name;
-        }
-    }
-    Human.prototype.greet = function (phrase) {
-        if (this.name) {
-            console.log("Hi, my name is " + this.name);
-        }
-        else {
-            console.log('Hi.');
-        }
-    };
-    return Human;
-}());
-var human = new Human('Oybek');
-var human1 = new Human('Oybek');
-var human2 = new Human('Husen');
-human2.greet('hi');
+var button = document.getElementById('myBtn');
+button.addEventListener('click', function (event) {
+    console.log(event.target);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,5 @@
-interface Named {
-    name?: string
-}
+const button = <HTMLButtonElement>document.getElementById('myBtn')
 
-interface Greetable extends Named{
-    name?: string
-    greet(phrase: string): void
-}
-
-class Human implements Greetable {
-    constructor(public name?: string) {
-        if(name) {
-            this.name = name
-        }
-    }
-    greet(phrase: string) {
-        if(this.name) {
-            console.log(`Hi, my name is ${this.name}`)
-        } else {
-            console.log('Hi.')
-        }
-    }
-}
-
-const human = new Human('Oybek')
-
-const human1: Greetable = new Human('Oybek')
-const human2: Greetable = new Human('Husen')
-
-human2.greet('hi')
+button.addEventListener('click', function (event) {
+    console.log(event.target)
+})
